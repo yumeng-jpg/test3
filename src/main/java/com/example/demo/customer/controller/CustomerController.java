b6a698882cf39219802026e56bd1080567c74c5b
yu <2960344031@qq.com>
2020-09-11 19:44:02 +0800

    第二次测试

---
+++ src/main/java/com/example/demo/customer/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.example.demo.customer.controller;
+
+/**
+ * @Author ym
+ * @Date 2020/9/7 15:08
+ * @Version 1.0
+ */
+public class CustomerController {
+}
---
+++ src/main/java/com/example/demo/customer/dao/CustomerMapper.java
@@ -0,0 +1,9 @@
+package com.example.demo.customer.dao;
+
+/**
+ * @Author ym
+ * @Date 2020/9/7 15:10
+ * @Version 1.0
+ */
+public interface CustomerMapper {
+}
